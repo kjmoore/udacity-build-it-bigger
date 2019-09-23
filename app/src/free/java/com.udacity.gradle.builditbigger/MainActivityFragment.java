fef1e20b2c1d68a2da266e3fa084cfb06fec7de6
Kieran Moore <kieranjohnmoore@gmail.com>
2019-09-24 00:32:07 +0100

    Adding in free and paid varients of the app

--- app/src/free/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/free/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
